7df08854e55d00b513c8c1e9c9f6e9d2e0e5edd7
xxemmexx <emme@pm.me>
2023-02-19 16:10:40 +0100

    Store data in local DB

--- app/global.R
+++ app/global.R
@@ -1,2 +1,21 @@
 require(shiny)
 require(shinydashboard)
+require(dplyr)
+require(DBI)
+require(RSQLite)
+
+localDBPath <- "data/nyc.sqlite3"
+
+# Connect to database when app is launched
+conn <- dbConnect(RSQLite::SQLite(), dbname = localDBPath)
+
+# Close database connection when app is closed
+shiny::onStop(function() {
+  dbDisconnect(conn)
+})
+
+# Turn off scientific notation
+options(scipen = 999)
+
+# Set spinner type (for loading)
+options(spinner.type = 4)
