f05f1b073dd047f516e2d1f59d2ab178a275335a
xxemmexx <emme@pm.me>
2023-02-25 19:25:59 +0100

    Added db_setup and model script

---
+++ data/db_setup.R
@@ -0,0 +1,74 @@
+#-------------------------------------------------------------------------------
+# Requirements
+#-------------------------------------------------------------------------------
+require(nycflights13)
+require(dplyr)
+require(DBI)
+require(RSQLite)
+
+#-------------------------------------------------------------------------------
+# Queries
+#-------------------------------------------------------------------------------
+
+drop_airlines_query <- "DROP TABLE IF EXISTS airlines"
+drop_airports_query <- "DROP TABLE IF EXISTS airports"
+drop_flights_query <- "DROP TABLE IF EXISTS flights"
+drop_planes_query <- "DROP TABLE IF EXISTS planes"
+drop_weather_query <- "DROP TABLE IF EXISTS weather"
+
+#-------------------------------------------------------------------------------
+# Extract
+#-------------------------------------------------------------------------------
+
+dataAirlines <- nycflights13::airlines
+dataAirports <- nycflights13::airports
+dataFlights <- nycflights13::flights
+dataPlanes <- nycflights13::planes
+dataWeather <- nycflights13::weather
+
+#-------------------------------------------------------------------------------
+# Transform 
+#-------------------------------------------------------------------------------
+
+dfWeather <- dataWeather %>%
+  mutate(time_hour = as.character(time_hour))
+
+
+ids <- tibble(flight_id = seq(from = 1, to = nrow(dataFlights)))
+
+dfFlights <- ids %>%
+  bind_cols(dataFlights) %>%
+  mutate(time_hour = as.character(time_hour)) 
+
+
+
+
+# str(dfFlights)
+# 
+# dfFlights %>%
+#   arrange(desc(year)) %>%
+#   head(10)
+
+
+#-------------------------------------------------------------------------------
+# Load 
+#-------------------------------------------------------------------------------
+
+conn <- dbConnect(RSQLite::SQLite(), dbname = "nyc.sqlite3")
+
+dbExecute(conn, drop_weather_query)
+dbWriteTable(conn, "weather", dfWeather)
+
+dbExecute(conn, drop_planes_query)
+dbWriteTable(conn, "planes", dataPlanes)
+
+dbExecute(conn, drop_flights_query)
+dbWriteTable(conn, "flights", dfFlights)
+
+dbExecute(conn, drop_airports_query)
+dbWriteTable(conn, "airports", dataAirports)
+
+dbExecute(conn, drop_airlines_query)
+dbWriteTable(conn, "airlines", dataAirlines)
+
+dbDisconnect(conn)
---
+++ data/model/vertraging_model_I.R
@@ -0,0 +1,107 @@
+#-------------------------------------------------------------------------------
+# Requirements
+#-------------------------------------------------------------------------------
+require(randomForest)
+require(dplyr)
+require(DBI)
+require(RSQLite)
+require(lubridate)
+
+#-------------------------------------------------------------------------------
+# Function to fetch data
+#-------------------------------------------------------------------------------
+fetchData <- function() {
+  conn <- dbConnect(RSQLite::SQLite(), dbname = 'app/data/nyc.sqlite3')
+  data <- dbGetQuery(conn, 
+                     "SELECT origin, sched_dep_time, arr_delay, p.tailnum, month, distance, dest, carrier, time_hour, wind_speed, wind_dir, visib, temp, dewp, humid, precip, pressure, manufacturer, seats
+                      FROM
+                      (
+                      SELECT f.origin, sched_dep_time, arr_delay, f.tailnum, f.month, f.distance, dest, carrier, f.time_hour, wind_speed, wind_dir, visib, w.temp, dewp, humid, precip, pressure
+                      FROM flights f 
+                      INNER JOIN weather w ON w.time_hour = f.time_hour
+                      ) j
+                      INNER JOIN planes p ON j.tailnum = p.tailnum;  ")
+  dbDisconnect(conn)
+  
+  return(data)
+}
+
+#-------------------------------------------------------------------------------
+# Load data
+#-------------------------------------------------------------------------------
+dataFlights <- fetchData()
+
+#-------------------------------------------------------------------------------
+# Pre-processing
+#-------------------------------------------------------------------------------
+#str(dataWeather)
+
+flightsDataset <- dataFlights %>%
+  transmute(origin = as.factor(origin),
+            month, 
+            sched_dep_time,
+            distance, 
+            carrier = as.factor(carrier), 
+            wind_speed,
+            wind_dir,
+            visib,
+            temp,
+            dewp,
+            humid, 
+            precip,
+            pressure,
+            manufacturer = as.factor(manufacturer),
+            seats,
+            arr_delay) %>%
+  na.omit()
+
+mask <- runif(n = 8000, min = 1, max = 336776) %>% unique() %>% as.integer()
+flightsDataset <- flightsDataset[mask,] %>% na.omit()
+
+#str(flightsDataset)
+
+for(i in 1:ncol(flightsDataset)) {
+  
+  thisValue <- sum(!complete.cases(flightsDataset[[i]]))
+  
+  if(thisValue > 0) {
+    print(paste0("Variable ", 
+                 names(flightsDataset)[[i]], 
+                 " has ", 
+                 thisValue,
+                 " NA values"))
+  }
+}
+#-------------------------------------------------------------------------------
+# Fit
+#-------------------------------------------------------------------------------
+#make this example reproducible
+set.seed(1)
+
+#fit the random forest model
+vertraging_model <- randomForest(
+  formula = arr_delay ~ .,
+  data = flightsDataset
+)
+
+#vertraging_model
+#-------------------------------------------------------------------------------
+# Export importance
+#-------------------------------------------------------------------------------
+#produce variable importance plot
+varImpPlot(vertraging_model) 
+
+write.csv2(vertraging_model$importance, "app/data/importance_model_I.csv")
+
+#tune the model
+# model_tuned <- tuneRF(
+#   x=airquality[,-1], #define predictor variables
+#   y=airquality$Ozone, #define response variable
+#   ntreeTry=500,
+#   mtryStart=4, 
+#   stepFactor=1.5,
+#   improve=0.01,
+#   trace=FALSE #don't show real-time progress
+# )
+
+
