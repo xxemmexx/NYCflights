be824cbb49be825f3a0a53624fcaadeb7ae5f209
xxemmexx <emme@pm.me>
2023-02-25 21:14:14 +0100

    Vertraging: run second type of origin-specific analysis. New files exported

--- data/model/vertraging_model.R
+++ data/model/vertraging_model.R
@@ -5,18 +5,17 @@ require(randomForest)
 require(dplyr)
 require(DBI)
 require(RSQLite)
-require(lubridate)
 
 #-------------------------------------------------------------------------------
-# Function to fetch data
+# Custom functions
 #-------------------------------------------------------------------------------
 fetchData <- function() {
   conn <- dbConnect(RSQLite::SQLite(), dbname = 'app/data/nyc.sqlite3')
   data <- dbGetQuery(conn, 
-                     "SELECT origin, sched_dep_time, arr_delay, p.tailnum, month, distance, dest, carrier, time_hour, wind_speed, wind_dir, visib, temp, dewp, humid, precip, pressure, manufacturer, seats
+                     "SELECT origin, sched_dep_time, dep_delay, arr_delay, p.tailnum, month, distance, dest, carrier, time_hour, wind_speed, wind_dir, visib, temp, dewp, humid, precip, pressure, manufacturer, seats
                       FROM
                       (
-                      SELECT f.origin, sched_dep_time, arr_delay, f.tailnum, f.month, f.distance, dest, carrier, f.time_hour, wind_speed, wind_dir, visib, w.temp, dewp, humid, precip, pressure
+                      SELECT f.origin, sched_dep_time, dep_delay, arr_delay, f.tailnum, f.month, f.distance, dest, carrier, f.time_hour, wind_speed, wind_dir, visib, w.temp, dewp, humid, precip, pressure
                       FROM flights f 
                       INNER JOIN weather w ON w.time_hour = f.time_hour
                       ) j
@@ -25,6 +24,14 @@ fetchData <- function() {
   
   return(data)
 }
+
+reduceDataset <- function(aDataset, aSample) {
+  mask <- runif(n = aSample, min = 1, max = nrow(aDataset)) %>% unique() %>% as.integer()
+  reducedDataset <- aDataset[mask,] %>% na.omit()
+  
+  return(reducedDataset)
+}
+
 
 #-------------------------------------------------------------------------------
 # Load data
@@ -35,7 +42,7 @@ dataFlights <- fetchData()
 # Pre-processing
 #-------------------------------------------------------------------------------
 
-# Feature selection
+# Feature selection model I
 flightsDataset <- dataFlights %>%
   transmute(origin = as.factor(origin),
             month, 
@@ -56,8 +63,77 @@ flightsDataset <- dataFlights %>%
   na.omit()
 
 # Take a small sample of the data to go quicker - at the expense of quality obviously...
-mask <- runif(n = 8000, min = 1, max = 336776) %>% unique() %>% as.integer()
-flightsDataset <- flightsDataset[mask,] %>% na.omit()
+flightsDataset <- reduceDataset(flightsDataset, 8000)
+
+
+# Feature selection model II - LGA
+LGADataset <- dataFlights %>%
+  filter(origin == 'LGA') %>%
+  transmute(month, 
+            sched_dep_time,
+            distance, 
+            carrier = as.factor(carrier), 
+            wind_speed,
+            wind_dir,
+            visib,
+            temp,
+            dewp,
+            humid, 
+            precip,
+            pressure,
+            manufacturer = as.factor(manufacturer),
+            seats,
+            dep_delay
+            ) %>%
+  na.omit()
+
+LGADataset <- reduceDataset(LGADataset, 8000)
+
+# Feature selection model II - JFK
+JFKDataset <- dataFlights %>%
+  filter(origin == 'JFK') %>%
+  transmute(month, 
+            sched_dep_time,
+            distance, 
+            carrier = as.factor(carrier), 
+            wind_speed,
+            wind_dir,
+            visib,
+            temp,
+            dewp,
+            humid, 
+            precip,
+            pressure,
+            manufacturer = as.factor(manufacturer),
+            seats,
+            dep_delay
+  ) %>%
+  na.omit()
+
+JFKDataset <- reduceDataset(JFKDataset, 8000)
+
+# Feature selection model II - EWR
+EWRDataset <- dataFlights %>%
+  filter(origin == 'EWR') %>%
+  transmute(month, 
+            sched_dep_time,
+            distance, 
+            carrier = as.factor(carrier), 
+            wind_speed,
+            wind_dir,
+            visib,
+            temp,
+            dewp,
+            humid, 
+            precip,
+            pressure,
+            manufacturer = as.factor(manufacturer),
+            seats,
+            dep_delay
+  ) %>%
+  na.omit()
+
+EWRDataset <- reduceDataset(EWRDataset, 8000)
 
 
 # Double-check no rows has NA values 
@@ -85,8 +161,26 @@ vertraging_model <- randomForest(
   data = flightsDataset
 )
 
+vertraging_model_lga <- randomForest(
+  formula = dep_delay ~ .,
+  data = LGADataset
+)
+
+vertraging_model_jfk <- randomForest(
+  formula = dep_delay ~ .,
+  data = JFKDataset
+)
+
+vertraging_model_ewr <- randomForest(
+  formula = dep_delay ~ .,
+  data = EWRDataset
+)
 #-------------------------------------------------------------------------------
 # Export importance
 #-------------------------------------------------------------------------------
 
 write.csv2(vertraging_model$importance, "app/data/importance_model_I.csv")
+
+write.csv2(vertraging_model_lga$importance, "app/data/importance_model_II_lga.csv")
+write.csv2(vertraging_model_jfk$importance, "app/data/importance_model_II_jfk.csv")
+write.csv2(vertraging_model_ewr$importance, "app/data/importance_model_II_ewr.csv")
